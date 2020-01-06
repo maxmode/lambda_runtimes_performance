42b0b99764c3aeb01742bc78635a9380d07870f1
maxmode <m.moskvychev@gmail.com>
2020-01-06 20:41:44 +0300

    Locust test updated

--- locustfile.py
+++ locustfile.py
@@ -3,20 +3,24 @@ from locust import HttpLocust, TaskSet, task
 class UserBehavior(TaskSet):
 
     @task(1)
-    def node12(self):
+    def node(self):
         self.client.get("/node12", name="node12")
 
     @task(1)
-    def python3(self):
+    def python(self):
         self.client.get("/python3.8", name="python3.8")
 
     @task(1)
-    def ruby2(self):
+    def ruby(self):
         self.client.get("/ruby2.5", name="ruby2.5")
 
     @task(1)
     def golang(self):
         self.client.get("/golang", name="golang")
+
+    @task(1)
+    def java(self):
+        self.client.get("/java11", name="java11")
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
